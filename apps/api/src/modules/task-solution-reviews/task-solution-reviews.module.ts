d7d89344f3b3b6a13dfcd7e1e4ecc352a96cda35
Mike Portnov <mikport26@gmail.com>
2025-05-20 13:40:35 +0800

    🐛 Bug fix for task-solution-reviews.module.ts

--- apps/api/src/modules/task-solution-reviews/task-solution-reviews.module.ts
+++ apps/api/src/modules/task-solution-reviews/task-solution-reviews.module.ts
@@ -5,6 +5,7 @@ import { CriterionScore } from './entities/criterion-score.entity';
 import { TaskSolutionReviewsService } from './task-solution-reviews.service';
 import { TaskSolutionReviewsController } from './task-solution-reviews.controller';
 import { TaskSolution } from '../task-solutions/entities/task-solution.entity';
+import { TaskSolutionsModule } from '../task-solutions/task-solutions.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TaskSolution } from '../task-solutions/entities/task-solution.entity';
       CriterionScore,
       TaskSolution,
     ]),
+    TaskSolutionsModule,
   ],
   providers: [TaskSolutionReviewsService],
   controllers: [TaskSolutionReviewsController],
--- apps/api/src/modules/task-solutions/task-solutions.module.ts
+++ apps/api/src/modules/task-solutions/task-solutions.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskSolution } from './entities/task-solution.entity';
 import { TaskSolutionsService } from './task-solutions.service';
 import { TaskSolutionsController } from './task-solutions.controller';
+import { SolutionSource } from './entities/solution-source.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskSolution])],
+  imports: [TypeOrmModule.forFeature([TaskSolution, SolutionSource])],
   providers: [TaskSolutionsService],
   controllers: [TaskSolutionsController],
   exports: [TaskSolutionsService],
