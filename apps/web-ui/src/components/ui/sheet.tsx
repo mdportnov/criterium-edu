c19571cfbeae22294c587ff61a3bae6127ace5be
Mike Portnov <mikport26@gmail.com>
2025-05-23 12:43:23 +0800

    🎨 new navigation, fixes for styles

--- apps/web-ui/src/components/ui/sheet.tsx
+++ apps/web-ui/src/components/ui/sheet.tsx
@@ -56,7 +56,7 @@ function SheetContent({
       <SheetPrimitive.Content
         data-slot="sheet-content"
         className={cn(
-          "bg-background border-border data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col shadow-2xl transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+          "bg-white dark:bg-gray-900 border-gray-200 dark:border-gray-700 data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col shadow-2xl transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
           side === "right" &&
             "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm",
           side === "left" &&
