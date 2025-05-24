4d43a4a254655d569586d0bb29c035234da9e632
Angoga <Anogeorgy05@gmail.com>
2025-05-24 19:53:08 +0300

    :ambulance: try dix deploy

--- apps/nginx/docker-entrypoint.sh
+++ apps/nginx/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '${BACKEND_PORT} ${FRONTEND_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_PORT:3000} ${FRONTEND_PORT:3001}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
